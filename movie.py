025f778e2cdf13b36dcc738bb444bce82ec47ce3
Jorge Rodriguez <jorger01@hotmail.es>
2021-01-30 13:40:26 +0100

    Subiendo clase movie

---
+++ movie.py
@@ -0,0 +1,30 @@
+class Movie ():
+
+    def __init__(self, title, releaseYear, nationality, genre):
+        self.title = title
+        self.releaseYear = releaseYear
+        self.nationality = nationality
+        self.genre = genre
+        self.actors = []
+        self.director = None
+        self.writer = None
+        self.language = ""
+        self.platform = ""
+        self.isMCU = False
+        self.mainCharacterName = ""
+        self.producer = ""
+        self.distributor = ""
+    
+    def mostrarMovie (self):
+        print("Title:", self.title)
+        print("Release year:", self.releaseYear)
+        for actor in actors:
+            actor.mostrarTodo()
+        director.mostrarTodo()
+        writer.mostrarTodo()
+        print("Language:", self.language)
+        print("Platform:", self.platform)
+        print("Is MCU:", self.isMCU)
+        print("Main Character Name:", self.mainCharacterName)
+        print("Producer:", self.producer)
+        print("Distributor:", self.distributor)
