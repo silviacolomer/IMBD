056b357f59cfdc1ffefa8e187ef6a0115cf4ddb5
silviacolomer <scolomergallo@gmail.com>
2021-01-30 13:36:32 +0100

    Actualización de Professionals

--- professional.py
+++ professional.py
@@ -14,3 +14,14 @@ class Professional() :
         self.oscarsNumber=oscarsNumber
         self.profession=profession
     
+    def mostrarTodo(self):
+        print('Su nombre es:', self.name)
+        print('Su edad es: ', self.edad)
+        print('Su género es:', self.genre)
+        print('Su color de pelo es: ', self. hairColour)
+        print('Su color de ojos es: ', self.eyeColour)
+        print('Su raza es: ', self.race)
+        print ('Su estado es: ', self.isRetired)
+        print('Su nacionalidad es: ', self.nationality)
+        print('Su número de oscars es: ', self.oscarsNumber)
+        print('Su profesión es: ', self.profession)
