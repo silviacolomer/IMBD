836219b2e4b892159c64a4e9eae49283c0d7e61e
silviacolomer <scolomergallo@gmail.com>
2021-01-30 13:27:04 +0100

    Primera modificación

---
+++ professional.py
@@ -0,0 +1,16 @@
+class Professional() :
+
+    def __init__(self, name, age, genre, weight, height, hairColour, eyeColour, race, isRetired, nationality, oscarsNumber, profession):
+        self.name = name
+        self.age= age
+        self.genre= genre
+        self.weight=weight
+        self.height=height
+        self.hairColour=hairColour
+        self.eyeColour=eyeColour
+        self.race=race
+        self.isRetired=isRetired
+        self.nationality=nationality
+        self.oscarsNumber=oscarsNumber
+        self.profession=profession
+    
