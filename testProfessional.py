92c82676495318770545d3d2db8ab2246db9ffc9
Jorge Rodriguez <jorger01@hotmail.es>
2021-01-30 13:59:56 +0100

    probando clase profesional

---
+++ testProfessional.py
@@ -0,0 +1,7 @@
+import professional as prof
+
+profesional3 = prof.Professional("Jorge", 28, "Masculino", 62, 179, "Castaño", "Marrón", "Blanca", False, "Español", 2, "Actor")
+profesional4 = prof.Professional("Alicia", 67, "Femenino", 55, 165, "Rubio", "Azul", "Blanca", True, "Colombiana", 3, "Directora")
+
+profesional3.mostrarTodo()
+# profesional4.mostrarTodo()
